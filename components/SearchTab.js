f90a883a6ddcc9cfeac2ff6ffe9cc9751316e864
Joshua Edo <joshua.edo01@gmail.com>
2023-03-21 07:11:38 +0000

    Update SearchTab.js

--- components/SearchTab.js
+++ components/SearchTab.js
@@ -6,7 +6,7 @@ export default function SearchTab() {
       <div className="flex w-full hover:shadow-lg focus-within:shadow-lg rounded-full border border-gray-200 px-3 md:px-5 py-2 md:py-3 items-center max-w-xs md:max-w-lg">
         <SearchIcon className="h-4 text-gray-400" />
 
-        <input type="text" className="focus:outline-none flex-grow" />
+        <input type="text" className="focus:outline-none flex-grow mx-1" />
 
         {/* Microphone */}
         <svg
