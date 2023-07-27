234f7b0d38b2569a33fb47a728461a7ddadc89c7
Joshua Edo <joshua.edo01@gmail.com>
2023-07-27 08:37:04 +0100

    Update HomeHeader.js

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -6,8 +6,8 @@ export default function HomeHeader() {
   return (
     <>
       <Head>
-        <title>Google</title>
-        <meta name="description" content="Google" />
+        <title>Search</title>
+        <meta name="description" content="Joshua Edo's Search Engine" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
